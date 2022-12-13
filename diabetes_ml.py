6ee76b8891001e4475bc5e8b33e925c02057cc7e
tongza331 <pst.tongxa@gmail.com>
2022-12-13 16:42:52 +0700

    select new feature

--- diabetes_ml.py
+++ diabetes_ml.py
@@ -137,13 +137,13 @@ elif app_mode=='Diabetes Prediction':
     vfl = st.number_input('Visceral Fat Level')
     bodyage = st.number_input('Body Age')
     
-    antihypertensive_op = st.selectbox(
-                'Ever patient use antihypertensive?',
-                ('Yes', 'No'))
-    if antihypertensive_op == 'Yes':
-        antihypertensive = 1
-    else:
-        antihypertensive = 0
+    # antihypertensive_op = st.selectbox(
+    #             'Ever patient use antihypertensive?',
+    #             ('Yes', 'No'))
+    # if antihypertensive_op == 'Yes':
+    #     antihypertensive = 1
+    # else:
+    #     antihypertensive = 0
 
     antidepressant_op = st.selectbox(
                 'Ever patient use antidepressant?',
@@ -169,13 +169,13 @@ elif app_mode=='Diabetes Prediction':
     else:
         antidiabetic = 0
 
-    smoking_op = st.selectbox(
-                'Ever patient smoking?',
-                ('Yes', 'No'))
-    if smoking_op == 'Yes':
-        smoking = 1
-    else:
-        smoking = 0
+    # smoking_op = st.selectbox(
+    #             'Ever patient smoking?',
+    #             ('Yes', 'No'))
+    # if smoking_op == 'Yes':
+    #     smoking = 1
+    # else:
+    #     smoking = 0
     
     drinking_op = st.selectbox(
                 'Ever patient drinking?',
@@ -185,21 +185,21 @@ elif app_mode=='Diabetes Prediction':
     else:
         drinking = 0
 
-    MesurementBloodPressure_op = st.selectbox(
-                'Everyday With or without blood pressure measurement',
-                ('With', 'Without'))
-    if MesurementBloodPressure_op == 'With':
-        MesurementBloodPressure = 1
-    else:
-        MesurementBloodPressure = 0
+    # MesurementBloodPressure_op = st.selectbox(
+    #             'Everyday With or without blood pressure measurement',
+    #             ('With', 'Without'))
+    # if MesurementBloodPressure_op == 'With':
+    #     MesurementBloodPressure = 1
+    # else:
+    #     MesurementBloodPressure = 0
     
-    eatingHabbit_op = st.selectbox(
-                'Eating habit',
-                ('3 meals a day', '2 meals a day'))
-    if eatingHabbit_op == '3 meals a day':
-        eatingHabbit = 1
-    else:
-        eatingHabbit = 0
+    # eatingHabbit_op = st.selectbox(
+    #             'Eating habit',
+    #             ('3 meals a day', '2 meals a day'))
+    # if eatingHabbit_op == '3 meals a day':
+    #     eatingHabbit = 1
+    # else:
+    #     eatingHabbit = 0
 
     preference_op = st.selectbox(
                 'Eating habit',
@@ -209,15 +209,15 @@ elif app_mode=='Diabetes Prediction':
     else:
         preference = 1
 
-    sleep_op = st.selectbox(
-                'Sleep state',
-                ('Sleeping well', 'Sleeping','Not sleep'))
-    if sleep_op == 'Sleeping':
-        sleep = 1
-    elif sleep_op == 'Sleeping well':
-        sleep = 2
-    else:
-        sleep = 3
+    # sleep_op = st.selectbox(
+    #             'Sleep state',
+    #             ('Sleeping well', 'Sleeping','Not sleep'))
+    # if sleep_op == 'Sleeping':
+    #     sleep = 1
+    # elif sleep_op == 'Sleeping well':
+    #     sleep = 2
+    # else:
+    #     sleep = 3
 
     anxietyHealth_op = st.selectbox(
                 'Anxiety health',
@@ -239,80 +239,77 @@ elif app_mode=='Diabetes Prediction':
     else:
         anxietyForgetful = 1
 
-    stairWithoutTransmitted_op = st.selectbox(
-                'Can up and down stairs without transmitted?',
-                ('Yes', 'No'))
-    if stairWithoutTransmitted_op == 'Yes':
-        stairWithoutTransmitted = 1
-    else:
-        stairWithoutTransmitted = 0
+    # stairWithoutTransmitted_op = st.selectbox(
+    #             'Can up and down stairs without transmitted?',
+    #             ('Yes', 'No'))
+    # if stairWithoutTransmitted_op == 'Yes':
+    #     stairWithoutTransmitted = 1
+    # else:
+    #     stairWithoutTransmitted = 0
 
-    walk15m_op = st.selectbox(
-                'Can walk for more than 15 minutes?',
-                ('Yes', 'No'))
-    if walk15m_op == 'Yes':
-        walk15m = 1
-    else:
-        walk15m = 0
+    # walk15m_op = st.selectbox(
+    #             'Can walk for more than 15 minutes?',
+    #             ('Yes', 'No'))
+    # if walk15m_op == 'Yes':
+    #     walk15m = 1
+    # else:
+    #     walk15m = 0
 
-    walkWithoutCane_op = st.selectbox(
-                'Can walk without a cane?',
-                ('Yes', 'No'))
-    if walkWithoutCane_op == 'Yes':
-        walkWithoutCane = 1
-    else:
-        walkWithoutCane = 0
+    # walkWithoutCane_op = st.selectbox(
+    #             'Can walk without a cane?',
+    #             ('Yes', 'No'))
+    # if walkWithoutCane_op == 'Yes':
+    #     walkWithoutCane = 1
+    # else:
+    #     walkWithoutCane = 0
 
-    goingout_op = st.selectbox(
-                'Actively going out',
-                ('Yes', 'No'))
-    if goingout_op == 'Yes':
-        goingout = 1
-    else:
-        goingout = 0
+    # goingout_op = st.selectbox(
+    #             'Actively going out',
+    #             ('Yes', 'No'))
+    # if goingout_op == 'Yes':
+    #     goingout = 1
+    # else:
+    #     goingout = 0
 
-    eatHardFood_op = st.selectbox(
-                'Can patient eat hard food?',
-                ('Yes', 'No'))
-    if eatHardFood_op == 'Yes':
-        eatHardFood = 1
-    else:
-        eatHardFood = 0
+    # eatHardFood_op = st.selectbox(
+    #             'Can patient eat hard food?',
+    #             ('Yes', 'No'))
+    # if eatHardFood_op == 'Yes':
+    #     eatHardFood = 1
+    # else:
+    #     eatHardFood = 0
 
-    hbp_op = st.selectbox(
-                'Have High blood pressure?',
-                ('Yes', 'No'))
-    if hbp_op == 'Yes':
-        hbp = 1
-    else:
-        hbp = 0
+    # hbp_op = st.selectbox(
+    #             'Have High blood pressure?',
+    #             ('Yes', 'No'))
+    # if hbp_op == 'Yes':
+    #     hbp = 1
+    # else:
+    #     hbp = 0
     
     bmi = (height*0.01)/bodyWeight
-    features = [gender,age,numResident,height,bodyWeight,strides,bfp,bm,smp,vfl,bodyage,bmi,antihypertensive,antidepressant
-                ,osteoporosis,antidiabetic,smoking,drinking,MesurementBloodPressure,eatingHabbit,preference
-                ,sleep,anxietyHealth,anxietyForgetful,stairWithoutTransmitted,walk15m,walkWithoutCane,
-                goingout,eatHardFood,hbp]
+    # features = [gender,age,numResident,height,bodyWeight,strides,bfp,bm,smp,vfl,bodyage,bmi,antihypertensive,antidepressant
+    #             ,osteoporosis,antidiabetic,smoking,drinking,MesurementBloodPressure,eatingHabbit,preference
+    #             ,sleep,anxietyHealth,anxietyForgetful,stairWithoutTransmitted,walk15m,walkWithoutCane,
+    #             goingout,eatHardFood,hbp]
+    features = [gender,age,numResident,height,bodyWeight,strides,bfp,bm,smp,vfl,bodyage,bmi,antidepressant
+                ,osteoporosis,antidiabetic,drinking,preference
+                ,anxietyHealth,anxietyForgetful]
 
     results = np.array(features).reshape(1, -1)
     if st.button("Predict"):
-        # lda = LDA(n_components=1)
-        # features_results = lda.transform(results)
-        picklefile = open("xgb_oversampling.pkl", "rb")
+    
+        picklefile = open("xgb_oversampling2.pkl", "rb")
         model = pickle.load(picklefile)
 
         prediction = model.predict_proba(results)
         label0 = prediction[0][0]
         label1 = prediction[0][1]
 
-        # string = 'Patient have risk diabetes: '+str(round(label1*(100),2))+'%'
-        # if label1 > 0.5:
-        #     st.error(string)
-        # else:
-        #     st.success(string)
 
         if label1 > label0:
             string = 'Patient have risk diabetes: '+str(round(label1*(100),2))+'%'
             st.error(string)
         else:
-            string = "Patient don't have risk diabetes" #: '+str(round(label0*(100),2))+'%'
+            string = "Patient don't have risk diabetes"
             st.success(string)
